82f6b468541b0c74a9d97773be7389d924615777
kevinnguyen0509 <Kevin.Nguyen0509@gmail.com>
2022-06-25 15:40:35 -0700

    Reorganized Default Css and Scripts files to their own vendor folders

--- MealPreppers/MealPreppers/App_Start/BundleConfig.cs
+++ MealPreppers/MealPreppers/App_Start/BundleConfig.cs
@@ -9,23 +9,24 @@ namespace MealPreppers
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/Vendor/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/Vendor/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/Vendor/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/Vendor/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/Vendor/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/font-awesome.min.css"));
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/font-awesome.css"));
         }
     }
 }
