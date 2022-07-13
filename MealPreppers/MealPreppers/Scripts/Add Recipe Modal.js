07d3595a754ca541a150864faf43730e3d83c6ed
Nasen Wilson <32120172+nasenw@users.noreply.github.com>
2022-07-13 00:25:40 -0700

    Add recipe modal made and Grid Changes

---
+++ MealPreppers/MealPreppers/Scripts/Add Recipe Modal.js
@@ -0,0 +1,16 @@
+﻿let modalSubmitButton = document.getElementById('modalSubmitButton');
+let previewTitle = document.getElementById('card-title-preview');
+
+modalSubmitButton.addEventListener('click', function() {
+    
+
+
+});
+
+let newText = document.getElementById('modal-input-title');
+
+newText.addEventListener('keyup', function () {
+    console.log(newText.value);
+    previewTitle.innerHTML = newText.value;
+
+});
