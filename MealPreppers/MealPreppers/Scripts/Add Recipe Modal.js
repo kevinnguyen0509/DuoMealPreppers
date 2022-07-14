6f3b18425af17e88a1ac78426c3e4297a77dbe8b
Nasen Wilson <32120172+nasenw@users.noreply.github.com>
2022-07-13 19:33:46 -0700

    Updated Modal to be more pog

--- MealPreppers/MealPreppers/Scripts/Add Recipe Modal.js
+++ MealPreppers/MealPreppers/Scripts/Add Recipe Modal.js
@@ -1,5 +1,9 @@
 ﻿let modalSubmitButton = document.getElementById('modalSubmitButton');
 let previewTitle = document.getElementById('card-title-preview');
+let previewDescription = document.getElementById('previewDescription');
+let previewImage = document.getElementById('previewImage');
+
+
 
 modalSubmitButton.addEventListener('click', function() {
     
@@ -7,10 +11,26 @@ modalSubmitButton.addEventListener('click', function() {
 
 });
 
-let newText = document.getElementById('modal-input-title');
+let newTitle = document.getElementById('modal-input-title');
 
-newText.addEventListener('keyup', function () {
-    console.log(newText.value);
-    previewTitle.innerHTML = newText.value;
+newTitle.addEventListener('keyup', function () {
+    console.log(newTitle.value);
+    previewTitle.innerHTML = newTitle.value;
+
+});
+
+let newDescription = document.getElementById('modal-description-preview');
+
+newDescription.addEventListener('keyup', function () {
+    console.log(newDescription.value);
+    previewDescription.innerHTML = newDescription.value;
+
+});
+
+let newImage = document.getElementById('modal-preview-image');
+
+newImage.addEventListener('keyup', function () {
+    console.log(newImage.value);
+    previewImage.src = newImage.value;
 
 });
