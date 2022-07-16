90c741a26856e4d62a6ed9172bb1919cc1c76f21
Nasen Wilson <32120172+nasenw@users.noreply.github.com>
2022-07-15 18:19:52 -0700

    turbo smurf v.2 - Grid receives all recipes from the database
    PROBLEM: Adding a new item is resulting in problems when coming back to the view, most likely an issue with how I am passing in the controller

---
+++ MealPreppers/MealPreppers/DataFactory/GetAllRecipes.cs
@@ -0,0 +1,75 @@
+﻿using MealPreppers.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MealPreppers.DataFactory
+{
+    public class GetAllRecipes
+    {
+        public List<RecipeItem> GrabRecipeList(RecipeItem recipeItem)
+        {
+
+            List<RecipeItem> recipeList = new List<RecipeItem>();
+            SqlConnection SQLConn = new SqlConnection();
+            SQLConn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["Meal_Preppers_Connections"].ConnectionString;
+            SQLConn.Open();
+            SqlCommand SQLComm = new SqlCommand("[sas].[GetAllMeals]", SQLConn);
+            SqlDataReader SQLRec;
+            SQLComm.CommandType = CommandType.StoredProcedure;
+
+            try
+            {
+
+                
+
+
+
+                SQLRec = SQLComm.ExecuteReader();
+
+
+
+                while (SQLRec.Read())
+                {
+
+                    recipeList.Add(new RecipeItem
+                    {
+
+                        ID = SQLRec.GetInt32(SQLRec.GetOrdinal("ID")),
+                        title = SQLRec.GetString(SQLRec.GetOrdinal("Title")),
+                        description = SQLRec.GetString(SQLRec.GetOrdinal("Description")),
+                        ingredients = SQLRec.GetString(SQLRec.GetOrdinal("Ingredients")),
+                        imageURL = SQLRec.GetString(SQLRec.GetOrdinal("ImageURL")),
+                        instructions = SQLRec.GetString(SQLRec.GetOrdinal("Instructions")),
+                        prepTime = SQLRec.GetInt32(SQLRec.GetOrdinal("PrepTime")),
+                        servings = SQLRec.GetInt32(SQLRec.GetOrdinal("Servings")),
+                        calories = SQLRec.GetInt32(SQLRec.GetOrdinal("Calories"))
+
+                    });
+
+
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                recipeList.Add(new RecipeItem
+                {
+                    title = "Oops, something went wrong!" + e.Message,
+                    description = "Failed"
+                });
+
+            }
+            finally
+            {
+                SQLConn.Close();
+            }
+
+            return recipeList;
+        }
+    }
+}
