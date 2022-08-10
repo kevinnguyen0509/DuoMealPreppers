fa53aa4aa28ad667c566ebe4a4001d0b33fd3458
Nasen Wilson <32120172+nasenw@users.noreply.github.com>
2022-08-10 01:51:38 -0700

    Work on Owned Ingredients

---
+++ MealPreppers/MealPreppers/Scripts/OwnedFoodItems.js
@@ -0,0 +1,12 @@
+﻿let filterList = $("#filterhaveIngredients");
+let filterItem = $("#haveIngredientsSearchInput");
+let filterList2 = document.getElementById("filterhaveIngredients");
+
+$("#addOwnedFood").click(function () {
+    filterList2.innerHTML = "";
+    console.log(filterList);
+    filterList2.innerHTML += '<button id="haveFood" class="btn" onclick="filterSelection("Lettuce")">Lettuce</button>';
+    //'<button id ="haveFood" class="btn" onclick="filterSelection('+filterItem.value+')>' + filterItem.value + '</button>';
+    console.log(filterList);
+
+});
